a7960abc2d2f106e18feba609f8475a4eb8d5084
Rozznow <robje408@student.liu.se>
2021-11-05 16:52:19 +0100

    end of day
    ad

--- TDDD49/MainWindow.xaml.cs
+++ TDDD49/MainWindow.xaml.cs
@@ -27,11 +27,7 @@ namespace TDDD49
 
         private void ButtonAddName_Click(object sender, RoutedEventArgs e)
         {
-            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
-            {
-                lstNames.Items.Add(txtName.Text);
-                txtName.Clear();
-            }
+
         }
     }
 }
