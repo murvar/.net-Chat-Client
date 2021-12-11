10eada1545c57a365adde71bbb4a27637d5891ea
Rozznow <robje408@student.liu.se>
2021-12-11 13:18:05 +0100

    Merge commit
    merge commit

--- TDDD49/obj/Debug/net6.0-windows/View/MainWindow.g.i.cs
+++ TDDD49/obj/Debug/net6.0-windows/View/MainWindow.g.i.cs
@@ -150,7 +150,7 @@ namespace TDDD49 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TDDD49;component/view/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TDDD49;V1.0.0.0;component/view/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
